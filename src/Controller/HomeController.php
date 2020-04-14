2d1b688b69ff8faa5135e396ae665682288fddad
Lorddistrict <etienne.crespi@laposte.net>
2020-04-14 21:03:27 +0200

    [Update][Controller] pull out Symfony structure & [Add] display data

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,16 +3,27 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Manager\OrganizationManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Yaml\Yaml;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/home", name="home")
      */
-    public function index()
+    public function index(OrganizationManager $organizationManager)
     {
-        return $this->render('home/home.html.twig');
+        $organizations = Yaml::parseFile('data/organizations.yaml');
+
+        $organizationsData = [];
+        foreach ($organizations['organizations'] as $organization) {
+            $organizationsData[] = $organizationManager->getProperties($organization);
+        }
+
+        return $this->render('home/home.html.twig', [
+            'organizations' => $organizationsData,
+        ]);
     }
 }
--- src/Manager/OrganizationManager.php
+++ src/Manager/OrganizationManager.php
@@ -3,7 +3,15 @@ declare(strict_types=1);
 
 namespace App\Manager;
 
+use App\Entity\Organization;
+
 class OrganizationManager
 {
-
+    /**
+     * Get properties of an Organization
+     */
+    public function getProperties(array $organization)
+    {
+        return new Organization($organization['name'], $organization['description']);
+    }
 }
